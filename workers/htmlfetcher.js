b76133c684cce0bd931459d662971b041691925e
chen101385 <chen1385@gmail.com>
2017-12-20 19:54:56 -0800

    Laid out pseudocode

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,12 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+//require archive-helpers module
+
+//run readListOfUrls to get list of urls
+
+//iterate through list of urls
+
+//run web crawler on urls that are false
+
+//flip url to true once web crawler is done
